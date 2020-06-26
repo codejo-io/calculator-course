01e67a51fc0254254e61d0ed0ee2337c7475379c
Matt Casanova <lazersquad@gmail.com>
2020-06-26 11:13:23 -0700

    Added ability to clear, do operations and change colors of buttons.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,13 +9,99 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let offColor = UIColor.systemOrange
+    let onColor = UIColor.orange
+    
 
+    @IBOutlet weak var divideButton: BorderedButton!
+    @IBOutlet weak var multiplicationButton: BorderedButton!
+    @IBOutlet weak var subtractButton: BorderedButton!
+    @IBOutlet weak var addButton: BorderedButton!
+    @IBOutlet weak var equalButton: BorderedButton!
+    
+    
+    @IBOutlet weak var displayLabel: UILabel!
+
+    var savedValue: Double = 0.0
+    var shouldReset = true
+    var displayString = "0"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        displayLabel.text = displayString
     }
 
-
+    @IBAction func onNumberClicked(_ sender: UIButton) {
+        guard let text = sender.currentTitle else { return }
+        
+        if text == "0" && displayString == "0" {
+            return
+        }
+        
+        if (shouldReset) {
+            displayString = text
+            shouldReset = false
+        } else {
+            displayString.append(text)
+        }
+        
+        displayLabel.text = displayString
+    }
+    
+    
+    @IBAction func onClearClicked(_ sender: UIButton) {
+        guard let text = sender.currentTitle else { return }
+        
+        if text == "AC" {
+            resetOperatorColors()
+            displayString = "0"
+            savedValue = 0
+        } else if text == "C" {
+            displayString = "0"
+        }
+        
+        shouldReset = true
+        displayLabel.text = displayString
+    }
+    
+    @IBAction func onOperatorClicked(_ sender: UIButton) {
+        
+        shouldReset = true
+        
+        if sender == equalButton {
+            processOperation()
+            resetOperatorColors()
+        } else if let value = Double(displayString) {
+            savedValue = value
+            resetOperatorColors()
+            sender.backgroundColor = onColor
+        }
+    }
+    
+    func processOperation() {
+        guard let value = Double(displayString) else { return }
+        
+        if addButton.backgroundColor == onColor {
+            displayString = "\(value + savedValue)"
+        } else if subtractButton.backgroundColor == onColor {
+            displayString = "\(savedValue - value)"
+        } else if multiplicationButton.backgroundColor == onColor {
+            displayString = "\(value * savedValue)"
+        } else if divideButton.backgroundColor == onColor {
+            displayString = "\(savedValue / value)"
+        }
+        
+        displayLabel.text = displayString
+    }
+    
+    func resetOperatorColors() {
+        divideButton.backgroundColor = offColor
+        multiplicationButton.backgroundColor = offColor
+        subtractButton.backgroundColor = offColor
+        addButton.backgroundColor = offColor
+        equalButton.backgroundColor = offColor
+    }
+    
 }
 
